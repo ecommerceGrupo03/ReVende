d062fd131cb195d4e76a9e8dd850b34b536cb804
mariliatostesdev <mahtostes.dev@gmail.com>
2024-01-16 15:16:09 -0300

    added getters and setters

--- reVende/src/main/java/com/reVende/projeto/model/Categorias.java
+++ reVende/src/main/java/com/reVende/projeto/model/Categorias.java
@@ -22,4 +22,29 @@ public class Categorias {
 	
 	@Size(max = 255, message = "Máximo de 255 caracteres para descrição")
 	private String descricao;
+	
+	public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+	
 }
