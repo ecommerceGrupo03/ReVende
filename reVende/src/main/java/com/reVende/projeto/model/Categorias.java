9f3b5dcee384a1183fe51ccec6d75ebdfae985fd
Leo <leonardo.r.s.duarte@gmail.com>
2024-01-16 14:28:32 -0300

    Script da classe Categorias na Model

--- reVende/src/main/java/com/reVende/projeto/model/Categorias.java
+++ reVende/src/main/java/com/reVende/projeto/model/Categorias.java
@@ -1,5 +1,25 @@
 package com.reVende.projeto.model;
 
-public class Categorias {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name = "tb_categorias")
+public class Categorias {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "Atributo nome é obrigatório")
+	@Size(min = 5, max = 255, message = "Minimo 5 e no máximo 255 caracteres para o atributo nome")
+	private String nome;
+	
+	@Size(max = 255, message = "Máximo de 255 caracteres para descrição")
+	private String descricao;
 }
