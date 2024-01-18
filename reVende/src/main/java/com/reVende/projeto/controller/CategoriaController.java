b7408b7f0a7e5877e73f4fd5f4e8f6bfb326ef87
marina-mmap <marinapereira.mmap@gmail.com>
2024-01-18 11:46:15 -0300

    Metodos FindAll e FindById

--- reVende/src/main/java/com/reVende/projeto/controller/CategoriaController.java
+++ reVende/src/main/java/com/reVende/projeto/controller/CategoriaController.java
@@ -23,7 +23,16 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
-	// Get
+	 @GetMapping
+	    public ResponseEntity<List<Categoria>> getAll(){
+	        return ResponseEntity.ok(categoriaRepository.findAll());
+	    }
+	    
+	 @GetMapping("/{id}")
+	    public ResponseEntity<Categoria> getById(@PathVariable Long id){
+	        return categoriaRepository.findById(id)
+	            .map(resposta -> ResponseEntity.ok(resposta))
+	            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	
 	@PostMapping
 	public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria) {
