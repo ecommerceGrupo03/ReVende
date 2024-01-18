780c2ff98566a2376c2da7e986ef78de0064f6fd
mariliatostesdev <mahtostes.dev@gmail.com>
2024-01-18 10:40:56 -0300

    added interface extend JpaRepository + findByDescricao()

---
+++ reVende/src/main/java/com/reVende/projeto/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.reVende.projeto.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.reVende.projeto.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+
+}
