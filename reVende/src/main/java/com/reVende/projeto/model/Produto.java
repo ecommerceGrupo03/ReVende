9dab4e868f634a3ef7deb20717b7d8d4ae354d6f
JaquePaula <jaquemarques.santos@gmail.com>
2024-01-19 10:25:18 -0300

    add atributos classe produto

--- reVende/src/main/java/com/reVende/projeto/model/Produto.java
+++ reVende/src/main/java/com/reVende/projeto/model/Produto.java
@@ -1,5 +1,94 @@
 package com.reVende.projeto.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_produtos")
+
 public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message= "O atributo nome é obrigatório")
+	@Size(min=10, max =255, message = "O atributo nome deve conter no minimo 10 e no maximo 255 caracteres")
+	private String nome;
+	
+	@NotBlank(message= "O atributo descrição é obrigatório")
+	@Size(min=10, max =255, message = "O atributo descrição deve conter no minimo 10 e no maximo 255 caracteres")
+	private String descricao;
+	
+	@NotNull
+	@Min(0)
+	private Long quantidade;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+    @NotNull(message = "Preço é obrigatório!")
+    @Min(0)
+    private Double preco;
+	
+	@Size(max =255, message = "O atributo foto deve conter no maximo 255 caracteres")
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Long quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
 
 }
