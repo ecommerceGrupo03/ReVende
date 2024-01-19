1c0a110bb6a3f618ee9d55aadb1100d56facd615
Leo <leonardo.r.s.duarte@gmail.com>
2024-01-19 10:03:52 -0300

    Atributos classe Model Usuario + métodos get e set

--- reVende/src/main/java/com/reVende/projeto/model/Usuario.java
+++ reVende/src/main/java/com/reVende/projeto/model/Usuario.java
@@ -1,5 +1,95 @@
 package com.reVende.projeto.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuarios")
 public class Usuario {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é de preenchimento obrigatório!")
+	@Size(max = 255, message = "Atributo nome recebe no máximo 255 caracteres")
+	private String nome;
+	
+	@NotBlank(message = "O atributo email é de preenchimento obrigatório!")
+	@Size(max = 255, message = "Atributo email recebe no máximo 255 caracteres")
+	private String email;
+	
+	@NotBlank(message = "O atributo senha é de preenchimento obrigatório!")
+	@Size(max = 255, message = "Atributo senha recebe no máximo 255 caracteres")
+	private String senha;
+	
+	private String foto;
+	
+	@Size(max = 14, message = "Atributo cpf recebe no máximo 14 caracteres")
+	private String cpf;
+	
+	@Size(max = 18, message = "Atributo cnpj recebe no máximo 18 caracteres")
+	private String cnpj;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
 }
