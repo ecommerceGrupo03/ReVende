3bcb71cb20625bd758213afd2b22d5eaadee732b
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-01-24 13:39:50 -0300

    Espaçamentos maiores na classe UsuarioLogin

--- reVende/src/main/java/com/reVende/projeto/model/UsuarioLogin.java
+++ reVende/src/main/java/com/reVende/projeto/model/UsuarioLogin.java
@@ -10,53 +10,67 @@ public class UsuarioLogin {
 	private String cpf;
 	private String cnpj;
 	private String token;
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getSenha() {
 		return senha;
 	}
+	
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+	
 	public String getFoto() {
 		return foto;
 	}
+	
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
 	public String getCpf() {
 		return cpf;
 	}
+	
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+	
 	public String getCnpj() {
 		return cnpj;
 	}
+	
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
+	
 	public String getToken() {
 		return token;
 	}
+	
 	public void setToken(String token) {
 		this.token = token;
 	} 
-	
 }
