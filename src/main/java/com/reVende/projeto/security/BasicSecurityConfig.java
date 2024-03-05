6ad7db9d2b5cb7df253671ceb3d6a93e7a44a142
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-03-05 18:27:19 -0300

    Update BasicSecurityConfig.java

--- src/main/java/com/reVende/projeto/security/BasicSecurityConfig.java
+++ src/main/java/com/reVende/projeto/security/BasicSecurityConfig.java
@@ -64,14 +64,16 @@ public class BasicSecurityConfig {
 	                .requestMatchers("/usuarios/logar").permitAll()
 	                .requestMatchers("/usuarios/cadastrar").permitAll()
 	                
-	                .requestMatchers(HttpMethod.GET, "/produtos/listar").permitAll()
+	                .requestMatchers("/produtos/listar").permitAll()
+	                .requestMatchers("/produtos/listar/*").permitAll()
 	                .requestMatchers("/produtos/nome/*").permitAll()
 	                .requestMatchers("/produtos/descricao/*").permitAll()
 	                .requestMatchers("/produtos/preco/*").permitAll()
 	                .requestMatchers("/produtos/preco/max/*").permitAll()
 	                .requestMatchers("/produtos/preco/entre/*/*").permitAll()
 	                
-	                .requestMatchers(HttpMethod.GET, "/categorias/listar").permitAll()
+	                .requestMatchers("/categorias/listar").permitAll()
+	                .requestMatchers("/categorias/listar/*").permitAll()
 	                .requestMatchers("/categorias/nome/*").permitAll()
 	                .requestMatchers("/categorias/descricao/*").permitAll()
 	                
